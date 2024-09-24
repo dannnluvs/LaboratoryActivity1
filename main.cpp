723d08af79b1b7fd125459308a27d9d4791c4ea7
dannnluvs <110661011+dannnluvs@users.noreply.github.com>
2024-09-24 17:48:26 +0800

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    //Sum
+    int firstNumber, secondNumber, sum;
+    
+    cout << "Enter First Number: ";
+    cin >> firstNumber;
+    
+    cout << "Enter Second Number: ";
+    cin >> secondNumber;
+    
+    sum = firstNumber + secondNumber;
+    
+    cout << "The Sum is " << sum;
+    
+    
+    //Difference
+    // int firstNumber, secondNumber, diff;
+    
+    // cout << "Enter First Number: ";
+    // cin >> firstNumber;
+    
+    // cout << "Enter Second Number: ";
+    // cin >> secondNumber;
+    
+    // diff = firstNumber - secondNumber;
+    
+    // cout << "The Difference is " << diff;
+    
+    
+    //Product
+    // int firstNumber, secondNumber, product;
+    
+    // cout << "Enter First Number: ";
+    // cin >> firstNumber;
+    
+    // cout << "Enter Second Number: ";
+    // cin >> secondNumber;
+    
+    // product = firstNumber * secondNumber;
+    
+    // cout << "The Product is " << product;
+    
+    
+    //Quotient
+    // int firstNumber, secondNumber, quotient;
+    
+    // cout << "Enter First Number: ";
+    // cin >> firstNumber;
+    
+    // cout << "Enter Second Number: ";
+    // cin >> secondNumber;
+    
+    // quotient = firstNumber / secondNumber;
+    
+    // cout << "The Quotient is " << quotient;
+    
+    return 0;
+    
+}
